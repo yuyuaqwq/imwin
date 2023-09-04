b5c969b222be904afc5590ac5c5549f4c56dff68
yuyuaqwq <1454832774@qq.com>
2023-09-04 16:08:47 +0800

    first commit

---
+++ import_win32.h
@@ -0,0 +1,18 @@
+#ifndef IMGUI_IMPORT_WIN32_H_
+#define IMGUI_IMPORT_WIN32_H_
+
+#include <imgui/imgui.h>
+#include <imgui/imconfig.h>
+#include <imgui/imgui_internal.h>
+#include <imgui/imstb_rectpack.h>
+#include <imgui/imstb_textedit.h>
+
+#include <imgui/backends/imgui_impl_win32.h>
+#include <imgui/backends/imgui_impl_dx11.h>
+#include <d3d11.h>
+
+void ImGuiInit(ImGuiIO* io);
+void ImGuiUpdate(ImGuiIO* io);
+
+
+#endif // IMGUI_IMPORT_WIN32_H_
