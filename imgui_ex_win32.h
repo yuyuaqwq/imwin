085535d926e49cc65b64e6ef6f6a33008224b02a
yuyuaqwq <1454832774@qq.com>
2023-09-08 18:35:48 +0800

    .

--- imgui_ex_win32.h
+++ imgui_ex_win32.h
@@ -3,6 +3,7 @@
 
 #include <unordered_map>
 #include <string>
+#include <exception>
 
 
 #ifndef IMGUI_EX_CPP
@@ -54,61 +55,94 @@ static bool SetWindowTop(ImGuiWindow* window, bool top) {
 
 
 #define IMGUI_EX_EXPAND_UNIT_DECLARATION() \
-    bool old_expand_; \
+    bool end_expand_; \
     bool expand_; \
 
 #define IMGUI_EX_EXPAND_UNIT_EVENT_DEFINITION() \
     template <typename EventBlock> \
     void ExpandEvent(EventBlock expand) { \
-        if (expand_ == true && expand_ != old_expand_) { \
+        if (expand_ == true && expand_ != end_expand_) { \
             expand(); \
         } \
     } \
     \
     template <typename EventBlock> \
     void CollapsingEvent(EventBlock collapsing) { \
-        if (expand_ == false && expand_ != old_expand_) { \
+        if (expand_ == false && expand_ != end_expand_) { \
             collapsing(); \
         } \
     } \
 
-#define IMGUI_EX_EXPAND_UNIT_UPDATE_DEFINITION() \
+#define IMGUI_EX_EXPAND_UNIT_UPDATE_DEFINITION(exp) \
     template <typename UpdateBlock> \
     void ExpandUpdate(UpdateBlock expand) { \
-        if (expand_ == true) { \
+        if (exp expand_ == true) { \
             expand(); \
         } \
     } \
     \
     template <typename UpdateBlock> \
     void CollapsingUpdate(UpdateBlock collapsing) { \
-        if (expand_ == false) { \
+        if (exp expand_ == false) { \
             collapsing(); \
         } \
     } \
 
+
+/*
+* Event设计原则
+* 当前帧对组件的Control，所有事件都要留到下一帧才能触发
+*/
+
 class Unit {
 public:
     Unit(const std::string& label) : label_(label) {
+        entry_disabled_ = false;
         end_disabled_ = false;
         disabled_ = false;
+        init_ = false;
     }
 
     void Begin() {
         if (disabled_) {
             ImGui::BeginDisabled();
-            end_disabled_ = true;
+            entry_disabled_ = true;
         }
         else {
-            end_disabled_ = false;
+            entry_disabled_ = false;
         }
     }
 
     void End() {
-        if (end_disabled_) {
+        end_disabled_ = disabled_;
+        if (entry_disabled_) {
             ImGui::EndDisabled();
         }
     }
+
+
+    template <typename EventBlock>
+    void InitEvent(EventBlock init) {
+        if (!init_) {
+            init_ = true;
+            init();
+        }
+    }
+
+    template <typename EventBlock>
+    void DisableEvent(EventBlock event) {
+        if (disabled_ == true && disabled_ != end_disabled_) {
+            event();
+        }
+    }
+
+    template <typename EventBlock>
+    void EnableEvent(EventBlock event) {
+        if (disabled_ == false && disabled_ != end_disabled_) {
+            event();
+        }
+    }
+
 
     std::string GetLabel() {
         return label_;
@@ -122,9 +156,20 @@ public:
         disabled_ = disabled;
     }
 
+    void Disable() {
+        SetDisable(true);
+    }
+
+    void Enable() {
+        SetDisable(false);
+    }
+
 private:
     std::string label_;
 
+    bool init_;
+
+    bool entry_disabled_;
     bool end_disabled_;
     bool disabled_;
 };
@@ -133,52 +178,69 @@ private:
 
 class Window : public Unit {
 public:
-    Window(const std::string& label, bool main) : Unit(label) {
+    Window(const std::string& label, bool main = false, bool create = true, ImGuiWindowFlags flags = 0) : Unit(label) {
         window_ = nullptr;
         main_ = main;
-        old_top_ = true;
+        end_top_ = true;
         top_ = false;
-        old_create_ = false;
-        create_ = true;
+        control_create_ = false;
+        control_close_ = false;
+        end_create_ = false;
+        create_ = create;
 
-        old_expand_ = false;
+        end_expand_ = false;
         expand_ = false;
 
-        old_flags_ = 0;
-        flags_ = 0;
+        entry_ = false;
+
+        end_flags_ = flags;
+        flags_ = flags;
     }
 
     void Begin() {
         Unit::Begin();
+        
         if (window_ == nullptr) {
             window_ = ImGui::FindWindowByName(GetLabel().c_str());
         }
 
-        if (old_top_ != top_) {
+        if (end_top_ != top_) {
             if (internal::SetWindowTop(window_, top_)) {
-                old_top_ = top_;
+                end_top_ = top_;
             }
         }
 
+        if (control_create_) {
+            create_ = true;
+            control_create_ = false;
+        }
+        if (control_close_) {
+            create_ = false;
+            control_close_ = false;
+        }
+
         if (create_) {
+            entry_ = true;
             expand_ = ImGui::Begin(GetLabel().c_str(), &create_, flags_);
         }
-        if (create_ == true && create_ != old_create_) {
-            // OpenEvent
+        if (create_ == true && create_ != end_create_) {
             // 每次重新开启窗口都要重设top状态
-            old_top_ = !top_;
+            end_top_ = !top_;
         }
     }
 
     void End() {
-        old_create_ = create_;
+        if (entry_) {
+            ImGui::End();
+            entry_ = false;
+        }
+        end_create_ = create_;
         if (create_ == false) {
             if (main_) {
                 ExitApplication();
             }
         }
-        old_expand_ = expand_;
-        ImGui::End();
+        end_expand_ = expand_;
         Unit::End();
     }
 
@@ -199,20 +261,22 @@ public:
         }
     }
 
+    IMGUI_EX_EXPAND_UNIT_UPDATE_DEFINITION(create_ == true && );
 
     /*
     * Event
     */
+
     template <typename EventBlock>
     void CreateEvent(EventBlock open) {
-        if (old_create_ == false && create_ == true) {
+        if (end_create_ == false && create_ == true) {
             open();
         }
     }
 
     template <typename EventBlock>
     void CloseEvent(EventBlock close) {
-        if (old_create_ == true && create_ == false) {
+        if (end_create_ == true && create_ == false) {
             close();
         }
     }
@@ -223,12 +287,29 @@ public:
     /*
     * Control
     */
-    void Open() {
-        create_ = true;
+    void Create() {
+        control_create_ = true;
     }
 
     void Close() {
-        create_ = false;
+        control_close_ = true;
+    }
+
+
+    ImGuiWindowFlags GetFlags() {
+        return flags_;
+    }
+
+    void SetFlags(ImGuiWindowFlags flags) {
+        flags_ = flags;
+    }
+
+    void AddFlags(ImGuiWindowFlags flags) {
+        flags_ |= flags;
+    }
+
+    void UnaddFlags(ImGuiWindowFlags flags) {
+        flags_ &= (~flags);;
     }
 
 
@@ -238,10 +319,37 @@ public:
 
     void SetDocking(bool enable){
         if (enable) {
-            flags_ &= (~ImGuiWindowFlags_NoDocking);
+            UnaddFlags(ImGuiWindowFlags_NoDocking);
         }
         else {
-            flags_ |= ImGuiWindowFlags_NoDocking;
+            AddFlags(ImGuiWindowFlags_NoDocking);
+        }
+    }
+
+    void SetAlwaysAutoResize(bool enable) {
+        if (enable) {
+            AddFlags(ImGuiWindowFlags_AlwaysAutoResize);
+        }
+        else {
+            UnaddFlags(ImGuiWindowFlags_AlwaysAutoResize);
+        }
+    }
+
+    void SetMove(bool enable) {
+        if (enable) {
+            UnaddFlags(ImGuiWindowFlags_NoMove);
+        }
+        else {
+            AddFlags(ImGuiWindowFlags_NoMove);
+        }
+    }
+
+    void SetCollapse(bool enable) {
+        if (enable) {
+            UnaddFlags(ImGuiWindowFlags_NoCollapse);
+        }
+        else {
+            AddFlags(ImGuiWindowFlags_NoCollapse);
         }
     }
 
@@ -249,14 +357,18 @@ private:
     ImGuiWindow* window_;
 
     bool main_;
-    bool old_top_;
+    bool entry_;
+    bool end_top_;
     bool top_;
-    bool old_create_;
+
+    bool control_close_;
+    bool control_create_;
+    bool end_create_;
     bool create_;
 
     IMGUI_EX_EXPAND_UNIT_DECLARATION();
 
-    ImGuiWindowFlags old_flags_;
+    ImGuiWindowFlags end_flags_;
     ImGuiWindowFlags flags_;
 };
 
@@ -264,15 +376,19 @@ class Button : public Unit {
 public:
     Button(const std::string& label) : Unit(label) {
         click_ = false;
+        control_click_ = false;
     }
 
     void Begin() {
         Unit::Begin();
         click_ = ImGui::Button(GetLabel().c_str());
+        if (control_click_) {
+            click_ = true;
+            control_click_ = false;
+        }
     }
 
     void End() {
-        click_ = false;
         Unit::End();
     }
 
@@ -286,7 +402,16 @@ public:
         }
     }
 
+
+    /*
+    * Control
+    */
+    void Click() {
+        control_click_ = true;
+    }
+
 private:
+    bool control_click_;
     bool click_;
 };
 
@@ -294,10 +419,10 @@ template<class Element = std::string>
 class Combo : public Unit {
 public:
     Combo(const std::string& label) : Unit(label) {
-        old_select_index_ = -1;
+        end_select_index_ = -1;
         select_index_ = -1;
 
-        old_expand_ = false;
+        end_expand_ = false;
         expand_ = false;
     }
 
@@ -307,9 +432,11 @@ public:
     }
 
     void End() {
-        ImGui::EndCombo();
-        old_expand_ = expand_;
-        old_select_index_ = select_index_;
+        if (expand_) {
+            ImGui::EndCombo();
+        }
+        end_expand_ = expand_;
+        end_select_index_ = select_index_;
         Unit::End();
     }
 
@@ -318,7 +445,7 @@ public:
     */
     template <typename UpdateBlock>
     void InsertUpdate(UpdateBlock insert) {
-        if (expand_) {
+        if (expand_ == false) {
             return;
         }
         for (int i = 0; i < list_.size(); i++) {
@@ -348,7 +475,7 @@ public:
 
     template <typename EventBlock>
     void SelectEvent(EventBlock select) {
-        if (old_select_index_ != select_index_) {
+        if (end_select_index_ != select_index_) {
             select();
         }
     }
@@ -357,8 +484,24 @@ public:
     /*
     * Control
     */
+    Element& GetSelectItem() {
+        return list_.at(select_index_);
+    }
+
+    int GetSelectIndex() {
+        return select_index_;
+    }
+
+    int SetSelectIndex(int select_index) {
+        select_index_ = select_index;
+    }
+
     void SetList(std::vector<Element>&& list) {
         list_ = std::move(list);
+    }
+
+    std::vector<Element>& GetList() {
+        return list_;
     }
 
     void ClearList() {
@@ -367,7 +510,7 @@ public:
 
 private:
     std::vector<Element> list_;
-    int old_select_index_;
+    int end_select_index_;
     int select_index_;
     std::string select_label_;
 
@@ -378,7 +521,7 @@ private:
 class InputText : public Unit {
 public:
     InputText(const std::string& label, size_t text_size) : Unit(label), text_(text_size, '\0') {
-        old_input_ = false;
+        end_input_ = false;
         input_ = false;
     }
 
@@ -392,7 +535,7 @@ public:
     }
 
     void End() {
-        old_input_ = input_;
+        end_input_ = input_;
         Unit::End();
     }
 
@@ -401,7 +544,7 @@ public:
     */
     template <typename EventBlock>
     void InputEvent(EventBlock input) {
-        if (input_ == true && input_ != old_input_) {
+        if (input_ == true && input_ != end_input_) {
             input();
         }
     }
@@ -410,13 +553,103 @@ public:
         return std::string(text_.c_str(), strlen(text_.c_str()));
     }
 
+    void SetText(const std::string& text) {
+        memcpy((void*)text_.c_str(), text.c_str(), (text_.size() < text.size() ? text_.size() : text.size()));
+    }
+
 private:
     std::string text_;
 
-    bool old_input_;
+    bool end_input_;
     bool input_;
 };
 
+class InputTextMultiline : public Unit {
+public:
+    InputTextMultiline(const std::string& label, const std::string& text = "", ImGuiInputTextFlags flags = 0) : Unit(label), size_(-FLT_MIN, -FLT_MIN) {
+        end_input_ = false;
+        input_ = false;
+
+        flags_ = flags | ImGuiInputTextFlags_CallbackResize;
+        SetText(text);
+    }
+
+    void Begin() {
+        if (text_.empty()) {
+            text_.push_back('\0');
+        }
+        Unit::Begin();
+        if (ImGui::InputTextMultiline(GetLabel().c_str(), text_.begin(), text_.size(), size_, flags_, ResizeCallback, &text_)) {
+            input_ = true;
+        }
+        else {
+            input_ = false;
+        }
+    }
+
+    void End() {
+        end_input_ = input_;
+        Unit::End();
+    }
+
+    /*
+    * Event
+    */
+    template <typename EventBlock>
+    void InputEvent(EventBlock input) {
+        if (input_ == true && input_ != end_input_) {
+            input();
+        }
+    }
+
+    std::string GetText() {
+        return std::string(&text_[0], strlen(&text_[0]));
+    }
+
+    void SetText(const std::string& text) {
+        text_.clear();
+        text_.resize(text.size() + 1);
+        memcpy(&text_[0], &text[0], text.size() + 1);
+    }
+
+    void SetSize(const ImVec2& size) {
+        size_ = size;
+    }
+
+    ImVec2& GetSize() {
+        return size_;
+    }
+
+    void SetReadOnly(bool enable) {
+        if (enable) {
+            flags_ |= ImGuiInputTextFlags_ReadOnly;
+        }
+        else {
+            flags_ &= (~ImGuiInputTextFlags_ReadOnly);
+        }
+    }
+
+private:
+    static int ResizeCallback(ImGuiInputTextCallbackData* data) {
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackResize) {
+            ImVector<char>* my_str = (ImVector<char>*)data->UserData;
+            IM_ASSERT(my_str->begin() == data->Buf);
+            my_str->resize(data->BufSize); // NB: On resizing calls, generally data->BufSize == data->BufTextLen + 1
+            data->Buf = my_str->begin();
+        }
+        return 0;
+    }
+
+private:
+    ImVector<char> text_;
+
+    bool end_input_;
+    bool input_;
+
+    ImVec2 size_;
+
+    ImGuiInputTextFlags flags_;
+};
 
 class Text : public Unit {
 public:
@@ -433,6 +666,18 @@ public:
     void End() {
         Unit::End();
     }
+
+
+    std::string GetText() {
+        return GetLabel();
+    }
+
+    void SetText(const std::string& text) {
+        return SetLabel(text);
+    }
+
+private:
+    std::string text_;
 };
 
 class SeparatorText : public Unit {
@@ -498,7 +743,7 @@ private:
 class CollapsingHeader : public Unit {
 public:
     CollapsingHeader(const std::string& label) : Unit(label) {
-        old_expand_ = false;
+        end_expand_ = false;
         expand_ = false;
     }
 
@@ -508,7 +753,7 @@ public:
     }
 
     void End() {
-        old_expand_ = expand_;
+        end_expand_ = expand_;
         Unit::End();
     }
 
@@ -529,7 +774,7 @@ private:
 class TreeNode : public Unit {
 public:
     TreeNode(const std::string& label) : Unit(label) {
-        old_expand_ = false;
+        end_expand_ = false;
         expand_ = false;
     }
 
@@ -542,7 +787,7 @@ public:
         if (expand_) {
             ImGui::TreePop();
         }
-        old_expand_ = expand_;
+        end_expand_ = expand_;
         Unit::End();
     }
 
@@ -557,9 +802,9 @@ private:
 
 class CheckBox : public Unit {
 public:
-    CheckBox(const std::string& label) : Unit(label) {
-        old_check_ = false;
-        check_ = false;
+    CheckBox(const std::string& label, bool check = false) : Unit(label) {
+        end_check_ = false;
+        check_ = check;
     }
 
     void Begin() {
@@ -568,7 +813,7 @@ public:
     }
 
     void End() {
-        old_check_ = check_;
+        end_check_ = check_;
         Unit::End();
     }
 
@@ -578,14 +823,14 @@ public:
     */
     template <typename EventBlock>
     void CheckEvent(EventBlock check) {
-        if (old_check_ == false && check_ == true) {
+        if (end_check_ == false && check_ == true) {
             check();
         }
     }
 
     template <typename EventBlock>
     void UncheckEvent(EventBlock uncheck) {
-        if (old_check_ == true && check_ == false) {
+        if (end_check_ == true && check_ == false) {
             uncheck();
         }
     }
@@ -598,25 +843,28 @@ public:
     }
 
 private:
-    bool old_check_;
+    bool end_check_;
     bool check_;
 };
 
 template<class Element = std::string>
 class ListBox : public Unit {
 public:
-    ListBox(const std::string& label) : Unit(label) {
-        old_select_index_ = -1;
+    ListBox(const std::string& label) : Unit(label), size_(-FLT_MIN, -FLT_MIN) {
+        entry_ = false;
+        end_select_index_ = -1;
         select_index_ = -1;
     }
 
     void Begin() {
         Unit::Begin();
-        ImGui::BeginListBox(GetLabel().c_str());
+        entry_ = ImGui::BeginListBox(GetLabel().c_str(), size_);
     }
 
     void End() {
-        ImGui::EndListBox();
+        if (entry_) {
+            ImGui::EndListBox();
+        }
         Unit::End();
     }
 
@@ -648,7 +896,7 @@ public:
     */
     template <typename EventBlock>
     void SelectEvent(EventBlock select) {
-        if (old_select_index_ != select_index_) {
+        if (end_select_index_ != select_index_) {
             select();
         }
     }
@@ -656,8 +904,32 @@ public:
     /*
     * Control
     */
+    Element& GetSelectItem() {
+        return list_.at(select_index_);
+    }
+
+    int GetSelectIndex() {
+        return select_index_;
+    }
+
+    int SetSelectIndex(int select_index) {
+        select_index_ = select_index;
+    }
+
+    void SetSize(const ImVec2& size) {
+        size_ = size;
+    }
+
+    ImVec2& GetSize() {
+        return size_;
+    }
+
     void SetList(std::vector<Element>&& list) {
         list_ = std::move(list);
+    }
+
+    std::vector<Element>& GetList() {
+        return list_;
     }
 
     void ClearList() {
@@ -666,7 +938,12 @@ public:
 
 private:
     std::vector<Element> list_;
-    int old_select_index_;
+
+    ImVec2 size_;
+
+    bool entry_;
+
+    int end_select_index_;
     int select_index_;
 };
 
@@ -675,7 +952,7 @@ public:
     RadioButtonGroup(std::vector<std::string> label_list) : Unit(""), label_list_(label_list){
         push_index_ = 0;
         select_index_ = 0;
-        old_select_index_ = 0;
+        end_select_index_ = 0;
     }
 
     template <typename PushBlock>
@@ -693,7 +970,7 @@ public:
     }
 
     void End() {
-        old_select_index_ = select_index_;
+        end_select_index_ = select_index_;
         Unit::End();
     }
 
@@ -701,7 +978,7 @@ public:
 
     template <typename EventBlock>
     void SelectEvent(EventBlock select) {
-        if (old_select_index_ != select_index_) {
+        if (end_select_index_ != select_index_) {
             select();
         }
     }
@@ -710,10 +987,9 @@ private:
     std::vector<std::string> label_list_;
 
     int push_index_;
-    int old_select_index_;
+    int end_select_index_;
     int select_index_;
 };
-
 
 
 namespace layout {
